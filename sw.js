b3acb4ab75f2a53cf79d4a6a6dd48c932882ab66
9ine1ne._ <45345314+evernighter@users.noreply.github.com>
2026-05-01 23:27:15 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,19 @@
-const CACHE_NAME = 'sailor-fukuoka-v2';
+const CACHE_NAME = 'sailor-fukuoka-v4';
 const assets = [
-  './',             // 改為相對路徑
-  './index.html',   // 改為相對路徑
+  './',
+  './index.html',
+  './manifest.json',
   'https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@400;700&display=swap'
 ];
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(assets);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
   );
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
